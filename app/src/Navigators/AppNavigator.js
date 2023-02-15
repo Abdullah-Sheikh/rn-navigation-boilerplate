8a70d0462840c017af268fc0ca0ba8d61dbe0896
Abdullah Shaikh <abdullahshaikh@tk-lpt-0831.tkxel.com>
2023-02-15 15:44:46 +0500

    Drawer navigation

--- app/src/Navigators/AppNavigator.js
+++ app/src/Navigators/AppNavigator.js
@@ -2,19 +2,9 @@ import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import DrawerNavigator from './DrawerNavigator';
 
-function HomeScreen({navigation}) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-      title='Go to details'
-      onPress={() =>{
-        navigation.navigate('Details')
-      }}/>
-    </View>
-  );
-}
+
 
 function DetailsScreen() {
   return (
@@ -31,7 +21,7 @@ function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Drawer" component={DrawerNavigator} />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
